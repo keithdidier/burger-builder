0cdb20676110920cd42c9fc3a896dcf9c3848ad0
keithdidier <keith.b.didier@gmail.com>
2018-02-01 12:08:42 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import Layout from './components/Layout/Layout';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <p>Test</p>
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
